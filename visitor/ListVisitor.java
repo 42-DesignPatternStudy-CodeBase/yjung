0666d745b9a4cf6f7e63ccb84be998ec036386f5
jyw2671 <yjung@student.42seoul.kr>
2023-03-08 13:26:55 +0900

    feat: add ListVisit

---
+++ visitor/ListVisitor.java
@@ -0,0 +1,22 @@
+package visitor;
+import java.util.Iterator;
+
+public class ListVisitor extends Visitor {
+    private String dir = ""; // 현재 주목하고 있는 디렉토리명
+    @Override
+    public void visit(File file) {                  // 파일을 방문했을 때 호출된다.
+        System.out.println(dir + "/" + file);
+    }
+    @Override
+    public void visit(Directory directory) { // 디렉토리를 방문했을 때 호출된다.
+        System.out.println(dir + "/" + directory);
+        String savedir = dir;
+        dir = dir + "/" + directory.getName();
+        Iterator it = directory.iterator();
+        while (it.hasNext()) {
+            Entry entry = (Entry)it.next();
+            entry.accept(this);
+        }
+        dir = savedir;
+    }
+}
